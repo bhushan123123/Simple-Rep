22c673dd8a9c12b5d0b2e774198dc4e74553b8c6
Bhushan <bhushansamadur3@gmail.com>
2022-11-02 09:43:22 +0530

    code added

--- Test.java
+++ Test.java
@@ -1,8 +1,19 @@
 public class Test{
 
+<<<<<<< Updated upstream
 public void Add(){
 System.out.println(" Add method");
 }
 
  
+=======
+public static void main(String []args){
+
+for(int i=0;i<=10;i++){
+
+System.out.println(i);
+
+ }
+}
+>>>>>>> Stashed changes
 }
